775e3f47290b92f1c26eb7a7f16e1bc89decbf2e
Jonathan Laasonen <iphntm@iPHNTMs-MBP.lan>
2021-03-08 18:55:38 +0100

    fix

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -97,7 +97,7 @@ namespace PortfolioMVC.Areas.Identity.Pages.Account
                     //     protocol: Request.Scheme);  
   
                     // await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",  
-                    //     $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");  
+                    //     $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>."); 
   
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)  
                     {  
