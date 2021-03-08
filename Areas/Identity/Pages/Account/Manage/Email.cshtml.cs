fbd25e53cda6a07d4ab9af32dde2e516d094c6e6
Jonathan Laasonen <iphntm@iPHNTMs-MBP.lan>
2021-03-08 18:31:25 +0100

    fix app crash on email change

--- Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -92,19 +92,20 @@ namespace PortfolioMVC.Areas.Identity.Pages.Account.Manage
             if (Input.NewEmail != email)
             {
                 var userId = await _userManager.GetUserIdAsync(user);
-                var code = await _userManager.GenerateChangeEmailTokenAsync(user, Input.NewEmail);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                var callbackUrl = Url.Page(
-                    "/Account/ConfirmEmailChange",
-                    pageHandler: null,
-                    values: new { userId = userId, email = Input.NewEmail, code = code },
-                    protocol: Request.Scheme);
-                await _emailSender.SendEmailAsync(
-                    Input.NewEmail,
-                    "Confirm your email",
-                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                
+                // var code = await _userManager.GenerateChangeEmailTokenAsync(user, Input.NewEmail);
+                // code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                // var callbackUrl = Url.Page(
+                //     "/Account/ConfirmEmailChange",
+                //     pageHandler: null,
+                //     values: new { userId = userId, email = Input.NewEmail, code = code },
+                //     protocol: Request.Scheme);
+                // await _emailSender.SendEmailAsync(
+                //     Input.NewEmail,
+                //     "Confirm your email",
+                //     $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                StatusMessage = "Confirmation link to change email sent. Please check your email.";
+                // StatusMessage = "Confirmation link to change email sent. Please check your email.";
                 return RedirectToPage();
             }
 
